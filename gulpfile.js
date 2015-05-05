c4757332c3eecbf4fa4eef3ad2c1e2097d252067
Sam Enoka <slwenoka@gmail.com>
2015-05-05 10:10:46 +1000

    Update browsersync settings.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp        = require("gulp");
 var browserSync = require('browser-sync');
 
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', ['server', 'watch'], function() {
+  require('./metalsmith')();
+});
 
 gulp.task('server', function () {
   return browserSync.init(['dist/**/*'], {
-    server: { baseDir: './dist' }
+    server: { baseDir: './dist' },
+    open: false,
+    notify: false
   });
 });
 
