68487aec9da664d740f4090b8eeebff37add7ece
Sam Enoka <slwenoka@gmail.com>
2015-07-31 16:21:15 +1000

    Minor cleanup.

--- metalsmith.js
+++ metalsmith.js
@@ -9,38 +9,37 @@ var imagemin   = require('metalsmith-imagemin');
 var fs         = require('fs');
 var permalinks = require('metalsmith-permalinks');
 
+// Debug Helper. Type {{ debug }} to log current context.
+Handlebars.registerHelper("debug", function(optionalValue) {
+  console.log("Current Context");
+  console.log("====================");
+  console.log(this);
+  if (optionalValue) {
+    console.log("Value");
+    console.log("====================");
+    console.log(optionalValue);
+  }
+});
+
+// Register all partials in `template/partials`
+fs.readdirSync('templates/partials').forEach(function(file) {
+  var name = file.split(".")[0];
+  var contents = fs.readFileSync(__dirname + "/templates/partials/" + file).toString();
+  Handlebars.registerPartial(name, contents);
+});
+
+// If there's a prismic config use it.
+// Otherwise, run the build without it but show a notice.
+try {
+  var prismicConfig = JSON.parse(fs.readFileSync('./prismic-config.json'));
+  var prismicTask   = prismic(prismicConfig);
+} catch(err) {
+  var prismicTask = function() {
+    console.log('Note: Create a prismic-config.json to start pulling content from your Prismic.io repository.');
+  };
+}
 
 module.exports = function metalSmith(done) {
-  // Debug Helper. Type {{ debug }} to log current context.
-  Handlebars.registerHelper("debug", function(optionalValue) {
-    console.log("Current Context");
-    console.log("====================");
-    console.log(this);
-    if (optionalValue) {
-      console.log("Value");
-      console.log("====================");
-      console.log(optionalValue);
-    }
-  });
-
-  // Register all partials in `template/partials`
-  fs.readdirSync('templates/partials').forEach(function(file) {
-    var name = file.split(".")[0];
-    var contents = fs.readFileSync(__dirname + "/templates/partials/" + file).toString();
-    Handlebars.registerPartial(name, contents);
-  });
-
-  // If there's a prismic config use it.
-  // Otherwise, run the build without it but show a notice.
-  try {
-    var prismicConfig = JSON.parse(fs.readFileSync('./prismic-config.json'));
-    var prismicTask   = prismic(prismicConfig);
-  } catch(err) {
-    var prismicTask = function() {
-      console.log('Note: Create a prismic-config.json to start pulling content from your Prismic.io repository.');
-    };
-  }
-
   Metalsmith(__dirname)
     .clean(false)
     .use(changed())
