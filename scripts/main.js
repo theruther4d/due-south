63b65353c7772b0dd7982ae7ecd60c1d4940bb68
Joshua Rutherford <joshua.rutherford1@gmail.com>
2016-02-08 15:01:22 -0800

    async is causing dom ready to fail

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,7 @@ var imgix = require( './imgix' );
 var Main = ( function() {
     return {
         init: function() {
-            alert( 'initted!' );
             document.addEventListener( 'DOMContentLoaded', function( e ) {
-                alert( 'loaded!' );
                 // Do stuff with the images:
                 var images = new imgix();
                 images._init();
