4249dea382b035cc58540720dcef1a1c374c81a7
Josh Rutherford <joshua.rutherford1@gmail.com>
2016-02-08 23:02:34 +0000

    committing from devo.ps!

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -108,9 +108,7 @@ var imgix = require( './imgix' );
 var Main = ( function() {
     return {
         init: function() {
-            alert( 'initted!' );
             document.addEventListener( 'DOMContentLoaded', function( e ) {
-                alert( 'loaded!' );
                 // Do stuff with the images:
                 var images = new imgix();
                 images._init();
