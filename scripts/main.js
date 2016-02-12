41be45d1bc3c4bb714d4f92b2d8b8ea8fc2536c7
Joshua Rutherford <joshua.rutherford1@gmail.com>
2016-02-12 07:54:52 -0800

    hook up hamburger button

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,15 @@ var imgix = require( './imgix' );
 var Main = ( function() {
     return {
         init: function() {
+            var nav = document.querySelector( '.nav' );
+            var hamburger = document.querySelector( '.nav__utilities__item--hamburger' );
+
+            hamburger.addEventListener( 'click', function( e ) {
+                e.preventDefault();
+
+                nav.classList.toggle( 'nav--open' );
+            });
+
             document.addEventListener( 'DOMContentLoaded', function( e ) {
                 // Do stuff with the images:
                 var images = new imgix();
